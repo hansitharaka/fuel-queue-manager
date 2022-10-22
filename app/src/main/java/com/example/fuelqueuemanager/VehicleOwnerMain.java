e4fff17be8cb2643c349bcc069f567e860506723
Hansani Tharaka <66997469+hansitharaka@users.noreply.github.com>
2022-10-22 17:01:59 +0530

    Initial main UI for vehicle owners

---
+++ app/src/main/java/com/example/fuelqueuemanager/VehicleOwnerMain.java
@@ -0,0 +1,14 @@
+package com.example.fuelqueuemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleOwnerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_owner_main);
+    }
+}
